0c4935af2e2b8e299cec56a503d559e79bf5dff2
kaushalgosaliya5 <kaushalgosaliya5@gmail.com>
2025-02-24 10:54:07 +0530

    update files

--- data/src/main/java/com/example/data/remote/ApiService.kt
+++ data/src/main/java/com/example/data/remote/ApiService.kt
@@ -4,6 +4,6 @@ import com.example.domain.model.Item
 import retrofit2.http.GET
 
 interface ApiService {
-        @GET("9427c14a054baf569196") //@GET("866592d4df655060f42c")
+        @GET("9427c14a054baf569196")
         suspend fun getItemDetails() : List<Item>
 }
